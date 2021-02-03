32d5ac1667155ddd2d67ae082851b7f726a115c3
Alexa-crypto <78495955+Alexa-crypto@users.noreply.github.com>
2021-02-03 19:19:53 +0200

    Add files via upload

---
+++ LAB_1/LAB_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LAB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Для виходу, натисніть клавішу Enter...");
+            Console.ReadKey();
+        }
+    }
+}
