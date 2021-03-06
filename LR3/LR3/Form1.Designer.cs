eddfb201b205280fc55c3cc30b36d3e4242bfd3c
Alexa-crypto <78495955+Alexa-crypto@users.noreply.github.com>
2021-03-06 21:44:00 +0200

    Add files via upload

---
+++ LR3/LR3/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace LR3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.tb_x1 = new System.Windows.Forms.TextBox();
+            this.tb_x2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.less = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_x1
+            // 
+            this.tb_x1.Location = new System.Drawing.Point(143, 12);
+            this.tb_x1.Name = "tb_x1";
+            this.tb_x1.Size = new System.Drawing.Size(116, 22);
+            this.tb_x1.TabIndex = 0;
+            // 
+            // tb_x2
+            // 
+            this.tb_x2.Location = new System.Drawing.Point(143, 46);
+            this.tb_x2.Name = "tb_x2";
+            this.tb_x2.Size = new System.Drawing.Size(116, 22);
+            this.tb_x2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Змінна Х_1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Змінна Х_2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(293, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Результат у =";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(420, 28);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(40, 24);
+            this.result.TabIndex = 5;
+            this.result.Text = "ххх";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Розрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 49);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(213, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 49);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Вихід";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // less
+            // 
+            this.less.AutoSize = true;
+            this.less.Location = new System.Drawing.Point(307, 83);
+            this.less.Name = "less";
+            this.less.Size = new System.Drawing.Size(0, 17);
+            this.less.TabIndex = 9;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 154);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Blue;
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(483, 252);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Діапазон x=-1...30";
+            this.chart1.Titles.Add(title1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 417);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.less);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_x2);
+            this.Controls.Add(this.tb_x1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_x1;
+        private System.Windows.Forms.TextBox tb_x2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label less;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ LR3/LR3/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double y;
+            for (int x = -1; x <= 30; x++)
+            {
+                y = Math.Sqrt(56 * x + ((x + x + Math.Sin(x * x)) / (5 - Math.Cos(Math.Pow(x, 2)))));
+                chart1.Series[0].Points.AddXY(x, y);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_x1.Text) || (string.IsNullOrEmpty(tb_x2.Text))) { tb_x1.Text = tb_x2.Text = "ERROR_empty!!!"; return; }
+            double x1 = Convert.ToDouble(tb_x1.Text); 
+            double x2 = Convert.ToDouble(tb_x2.Text);
+            if (x1 < x2)
+                less.Text = "Число х1 менше, х1 = " + Convert.ToString(x1);
+            else if (x1 > x2)
+                less.Text = "Число х2 менше, х2 = " + Convert.ToString(x2);
+            else
+                less.Text = "Число х1 та х1 рівні та = " + Convert.ToString(x2);
+            
+            if (((5 - Math.Cos(Math.Pow(x2, 2))) != 0) && (56 * x1 + ((x1 + x2 + Math.Sin(x1 * x2)) / (5 - Math.Cos(Math.Pow(x2, 2))))) >= 0)
+            {
+                double y = Math.Sqrt(56 * x1 + ((x1 + x2 + Math.Sin(x1 * x2)) / (5 - Math.Cos(Math.Pow(x2, 2)))));
+                result.Text = y.ToString("0.###");
+            }
+            else { tb_x1.Text = tb_x2.Text = "ERROR x1_or_x2"; return; }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tb_x1.Text = string.Empty;
+            tb_x2.Text = string.Empty;
+            result.Text = "xxx";
+            less.Text = string.Empty;
+        }
+    }
+}
