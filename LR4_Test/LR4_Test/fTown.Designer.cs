87d577cf3fab9236baf7c74806193600d435faa9
Alexa-crypto <78495955+Alexa-crypto@users.noreply.github.com>
2021-03-20 13:31:11 +0200

    Add files via upload

---
+++ LR4_Test/LR4_Test/fTown.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace LR4_Test
+{
+    partial class fTown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbSquare = new System.Windows.Forms.TextBox();
+            this.tbYearIncome = new System.Windows.Forms.TextBox();
+            this.tbPopulation = new System.Windows.Forms.TextBox();
+            this.tbRegion = new System.Windows.Forms.TextBox();
+            this.tbCountry = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chbHasAirport = new System.Windows.Forms.CheckBox();
+            this.chbHasPort = new System.Windows.Forms.CheckBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbSquare);
+            this.groupBox1.Controls.Add(this.tbYearIncome);
+            this.groupBox1.Controls.Add(this.tbPopulation);
+            this.groupBox1.Controls.Add(this.tbRegion);
+            this.groupBox1.Controls.Add(this.tbCountry);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 286);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Загальні дані";
+            // 
+            // tbSquare
+            // 
+            this.tbSquare.Location = new System.Drawing.Point(190, 238);
+            this.tbSquare.Name = "tbSquare";
+            this.tbSquare.Size = new System.Drawing.Size(100, 22);
+            this.tbSquare.TabIndex = 11;
+            // 
+            // tbYearIncome
+            // 
+            this.tbYearIncome.Location = new System.Drawing.Point(190, 197);
+            this.tbYearIncome.Name = "tbYearIncome";
+            this.tbYearIncome.Size = new System.Drawing.Size(100, 22);
+            this.tbYearIncome.TabIndex = 10;
+            // 
+            // tbPopulation
+            // 
+            this.tbPopulation.Location = new System.Drawing.Point(190, 154);
+            this.tbPopulation.Name = "tbPopulation";
+            this.tbPopulation.Size = new System.Drawing.Size(100, 22);
+            this.tbPopulation.TabIndex = 9;
+            // 
+            // tbRegion
+            // 
+            this.tbRegion.Location = new System.Drawing.Point(190, 116);
+            this.tbRegion.Name = "tbRegion";
+            this.tbRegion.Size = new System.Drawing.Size(100, 22);
+            this.tbRegion.TabIndex = 8;
+            // 
+            // tbCountry
+            // 
+            this.tbCountry.Location = new System.Drawing.Point(190, 76);
+            this.tbCountry.Name = "tbCountry";
+            this.tbCountry.Size = new System.Drawing.Size(100, 22);
+            this.tbCountry.TabIndex = 7;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(190, 37);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 22);
+            this.tbName.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 243);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Площа (кв/км)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Річний дохід (грн)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Кількість мешканців";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Регіон";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Країна";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Назва міста"; 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chbHasAirport);
+            this.groupBox2.Controls.Add(this.chbHasPort);
+            this.groupBox2.Location = new System.Drawing.Point(12, 304);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(309, 119);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Транспорт";
+            // 
+            // chbHasAirport
+            // 
+            this.chbHasAirport.AutoSize = true;
+            this.chbHasAirport.Location = new System.Drawing.Point(9, 76);
+            this.chbHasAirport.Name = "chbHasAirport";
+            this.chbHasAirport.Size = new System.Drawing.Size(161, 21);
+            this.chbHasAirport.TabIndex = 2;
+            this.chbHasAirport.Text = "Місто має аеропорт";
+            this.chbHasAirport.UseVisualStyleBackColor = true;
+            // 
+            // chbHasPort
+            // 
+            this.chbHasPort.AutoSize = true;
+            this.chbHasPort.Location = new System.Drawing.Point(9, 37);
+            this.chbHasPort.Name = "chbHasPort";
+            this.chbHasPort.Size = new System.Drawing.Size(129, 21);
+            this.chbHasPort.TabIndex = 0;
+            this.chbHasPort.Text = "Місто має порт";
+            this.chbHasPort.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(343, 28);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(92, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "ОК";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(343, 57);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(92, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Скасувати";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // fTown
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(450, 439);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "fTown";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Дані про нове місто";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbSquare;
+        private System.Windows.Forms.TextBox tbYearIncome;
+        private System.Windows.Forms.TextBox tbPopulation;
+        private System.Windows.Forms.TextBox tbRegion;
+        private System.Windows.Forms.TextBox tbCountry;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chbHasAirport;
+        private System.Windows.Forms.CheckBox chbHasPort;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
