14e69d668e0616ff1bdd69255f68343ff142c748
Alexa-crypto <78495955+Alexa-crypto@users.noreply.github.com>
2021-04-07 17:32:37 +0300

    Add files via upload

---
+++ LR5_2/LR5_test_2/fMain.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace LR5_2
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAdd = new System.Windows.Forms.ToolStripButton();
+            this.btnEdit = new System.Windows.Forms.ToolStripButton();
+            this.tsSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnDel = new System.Windows.Forms.ToolStripButton();
+            this.btnClear = new System.Windows.Forms.ToolStripButton();
+            this.tsSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.gvPhone = new System.Windows.Forms.DataGridView();
+            this.bindSrcPhone = new System.Windows.Forms.BindingSource(this.components);
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvPhone)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindSrcPhone)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAdd,
+            this.btnEdit,
+            this.tsSeparator1,
+            this.btnDel,
+            this.btnClear,
+            this.tsSeparator2,
+            this.btnExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1006, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAdd.Image = global::LR5_test_2.Properties.Resources.btnAdd;
+            this.btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(24, 24);
+            this.btnAdd.Text = "Додати запис про місто";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnEdit.Image = global::LR5_test_2.Properties.Resources.btnEdit;
+            this.btnEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(24, 24);
+            this.btnEdit.Text = "Редагувати запис";
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // tsSeparator1
+            // 
+            this.tsSeparator1.Name = "tsSeparator1";
+            this.tsSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // btnDel
+            // 
+            this.btnDel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnDel.Image = global::LR5_test_2.Properties.Resources.btnDel;
+            this.btnDel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(24, 24);
+            this.btnDel.Text = "Видалити запис";
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnClear.Image = global::LR5_test_2.Properties.Resources.btnClear;
+            this.btnClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(24, 24);
+            this.btnClear.Text = "Очистити дані";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tsSeparator2
+            // 
+            this.tsSeparator2.Name = "tsSeparator2";
+            this.tsSeparator2.Size = new System.Drawing.Size(6, 27);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnExit.Image = global::LR5_test_2.Properties.Resources.btnExit;
+            this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(24, 24);
+            this.btnExit.Text = "Вийти з програми";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // gvPhone
+            // 
+            this.gvPhone.AllowUserToAddRows = false;
+            this.gvPhone.AutoGenerateColumns = false;
+            this.gvPhone.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvPhone.DataSource = this.bindSrcPhone;
+            this.gvPhone.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gvPhone.Location = new System.Drawing.Point(0, 27);
+            this.gvPhone.Name = "gvPhone";
+            this.gvPhone.ReadOnly = true;
+            this.gvPhone.RowHeadersWidth = 51;
+            this.gvPhone.RowTemplate.Height = 24;
+            this.gvPhone.Size = new System.Drawing.Size(1006, 423);
+            this.gvPhone.TabIndex = 1;
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 450);
+            this.Controls.Add(this.gvPhone);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "БД_Телефон";
+            this.Load += new System.EventHandler(this.fMain_Load);
+            this.Resize += new System.EventHandler(this.fMain_Resize);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvPhone)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindSrcPhone)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAdd;
+        private System.Windows.Forms.ToolStripButton btnEdit;
+        private System.Windows.Forms.ToolStripSeparator tsSeparator1;
+        private System.Windows.Forms.ToolStripButton btnDel;
+        private System.Windows.Forms.ToolStripButton btnClear;
+        private System.Windows.Forms.ToolStripSeparator tsSeparator2;
+        private System.Windows.Forms.ToolStripButton btnExit;
+        private System.Windows.Forms.BindingSource bindSrcPhone;
+        private System.Windows.Forms.DataGridView gvPhone;
+    }
+}
+
---
+++ LR5_2/LR5_test_2/fMain.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR5_2
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void fMain_Resize(object sender, EventArgs e)
+        {
+            int buttonsSize = 5 * btnAdd.Width + 2 * tsSeparator1.Width + 30;
+            btnExit.Margin = new Padding(Width - buttonsSize, 0, 0, 0);
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            gvPhone.AutoGenerateColumns = false;
+            DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "operatitong_sistem";
+            column.Name = "ОС";
+            gvPhone.Columns.Add(column);
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "cpu";
+            column.Name = "Центральний процесор";
+            gvPhone.Columns.Add(column);
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "gpu";
+            column.Name = "Графічний процесор";
+            gvPhone.Columns.Add(column);
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "rom";
+            column.Name = "ROM";
+            gvPhone.Columns.Add(column);
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "ram";
+            column.Name = "RAM";
+            gvPhone.Columns.Add(column);
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "tip_body";
+            column.Name = "Тип корпусу";
+            column.Width = 80;
+            gvPhone.Columns.Add(column);
+            column = new DataGridViewCheckBoxColumn();
+            column.DataPropertyName = "camera";
+            column.Name = "Камера";
+            column.Width = 60;
+            gvPhone.Columns.Add(column);
+            column = new DataGridViewCheckBoxColumn();
+            column.DataPropertyName = "biometrics";
+            column.Name = "Біометрія";
+            column.Width = 60;
+            gvPhone.Columns.Add(column);
+            EventArgs args = new EventArgs();
+            OnResize(args);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Phone phone = new Phone();
+            fAddPhone fphone = new fAddPhone(ref phone);
+            if (fphone.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcPhone.Add(phone);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            Phone phone = (Phone)bindSrcPhone.List[bindSrcPhone.Position];
+            fAddPhone ft = new fAddPhone(ref phone);
+            if (ft.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcPhone.List[bindSrcPhone.Position] = phone;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Видалити поточний запис?", "Видалення запису", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                bindSrcPhone.RemoveCurrent();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Очистити таблицю?\n\nВсі дані будуть втрачені", "Очищення даних", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                bindSrcPhone.Clear();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Закрити застосунок?", "Вихід з програми", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
